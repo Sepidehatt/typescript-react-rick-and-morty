e67a78eab0cb4e4e479a91cb35019f4bb116f36d
Sepideh Attar <sepideh.att@gmail.com>
2024-03-09 15:50:58 +0100

    implemented useFetchCharacters hook to fetch characters and pagination related properties

---
+++ src/hooks/useFetchCharacters.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+import { apiServices } from '../services/ApiService';
+import {
+  Character,
+  CharacterQueryParams,
+  CharacterResponse,
+} from '../interfaces/characters.interface';
+
+interface FetchCharactersParams {
+  searchTerm?: string;
+  currentPage: number;
+}
+
+const useFetchCharacters = ({
+  searchTerm,
+  currentPage,
+}: FetchCharactersParams) => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchCharacters = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const query: CharacterQueryParams = {
+        page: searchTerm ? 1 : currentPage,
+        name: searchTerm || '',
+      };
+
+      const response: CharacterResponse =
+        await apiServices.getCharacters(query);
+
+      setCharacters(response.results || []);
+      setTotalPages(response.info?.pages || 0);
+    } catch (error) {
+      console.error('Failed to fetch characters:', error);
+      
+      setCharacters([]);
+      setTotalPages(0);
+      setError(error instanceof Error ? error : new Error('An error occurred'));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const debouncedFetch = debounce(fetchCharacters, 500);
+    debouncedFetch();
+
+    return () => debouncedFetch.cancel();
+  }, [searchTerm, currentPage]);
+
+  return { characters, totalPages, loading, error };
+};
+
+export default useFetchCharacters;
