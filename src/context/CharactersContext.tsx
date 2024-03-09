33e8d7a7eace5158e807acc533d459e97b3a8b82
Sepideh Attar <sepideh.att@gmail.com>
2024-03-09 15:51:46 +0100

    added charactersContext to have access to characters data globally

---
+++ src/context/CharactersContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useState, useContext, ReactNode, FC } from 'react';
+import useFetchCharacters from '../hooks/useFetchCharacters';
+import { Character } from '../interfaces/characters.interface';
+
+interface CharactersContext {
+  characters: Character[];
+  totalPages: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+  setSearchTerm: (term: string) => void;
+  loading: boolean;
+  error: Error | null;
+}
+
+const CharactersContext = createContext<CharactersContext | undefined>(
+  undefined,
+);
+
+interface CharactersProviderProps {
+  children: ReactNode;
+}
+
+export const CharactersProvider: FC<CharactersProviderProps> = ({
+  children,
+}) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const { characters, totalPages, loading, error } = useFetchCharacters({
+    searchTerm,
+    currentPage,
+  });
+
+  const value = {
+    characters,
+    totalPages,
+    currentPage,
+    setCurrentPage,
+    setSearchTerm,
+    loading,
+    error,
+  };
+
+  return (
+    <CharactersContext.Provider value={value}>
+      {children}
+    </CharactersContext.Provider>
+  );
+};
+
+export const useCharacters = (): CharactersContext => {
+  const context = useContext(CharactersContext);
+  if (context === undefined) {
+    throw new Error('useCharacters must be used within a CharactersProvider');
+  }
+  return context;
+};
