b250ede7b3e73d642a2cc1a1b4288a2e270afdef
Sepideh Attar <sepideh.att@gmail.com>
2024-03-10 17:31:03 +0100

    Added Spinner for loading and alert for error message

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -8,8 +8,18 @@ const Characters = () => {
 
   return (
     <>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error fetching characters: {error.message}</p>}
+      {loading && (
+        <div className="d-flex justify-content-center align-items-center" style={{ position: 'fixed', top: 0, left: 0, height: '100vh', width: '100vw', background: 'rgba(0, 0, 0, 0.5)' }}>
+          <div className="spinner-border text-primary" role="status" style={{ width: '3rem', height: '3rem' }}>
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      )}
+      {error && 
+      <div className="alert alert-danger w-25" role="alert">
+        Error fetching characters: {error.message}
+        </div>
+      }
       <div className="table-responsive mx-auto my-3" style={{ maxWidth: '90%' }}> 
       <table className="table table-sm table-bordered">
         <thead>
