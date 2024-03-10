169bbc7ba35c4bf11e6501847a6ebc994dbaa232
Sepideh Attar <sepideh.att@gmail.com>
2024-03-10 15:54:41 +0100

    Added style to homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,16 +2,28 @@ import { CharactersProvider } from '../context/CharactersContext';
 import Characters from '../components/Characters';
 import CharactersPagination from '../components/CharactersPagination';
 import SearchBar from '../components/searchBar/SearchBar';
+import './Homepage.style.scss';
 
 const HomePage = () => {
   return (
     <CharactersProvider>
-      <>
-        <SearchBar />
-        <Characters />
-        <CharactersPagination />
-      </>
-    </CharactersProvider>
+    <div className="homepage-banner">
+      <h1>RICK & MORTY</h1>
+      <SearchBar />
+    </div>
+    <div className="container characters-content">
+      <div className="row">
+        <div className="col">
+          <Characters />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col d-flex justify-content-center">
+          <CharactersPagination />
+        </div>
+      </div>
+    </div>
+  </CharactersProvider>
   );
 };
 
