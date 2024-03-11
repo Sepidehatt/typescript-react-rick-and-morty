a5b8cbafcf6d47374612320443f463df99b5bf3f
Sepideh Attar <sepideh.att@gmail.com>
2024-03-11 15:58:44 +0100

    install and setup for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCommonjs(), visualizer()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
