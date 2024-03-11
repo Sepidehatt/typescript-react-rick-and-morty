444971d57cdca63f8f28f6efe346d5ce3a7c1cb2
Sepideh Attar <sepideh.att@gmail.com>
2024-03-11 11:00:03 +0100

    Fixed expand symbol style

--- src/components/characters/CharacterDetail.tsx
+++ src/components/characters/CharacterDetail.tsx
@@ -8,17 +8,20 @@ const CharacterDetail: React.FC<CharacterProps> = ({ character }) => {
   return (
     <tbody>
       <tr onClick={toggle} style={{ cursor: 'pointer' }}>
-        <td>
+        <td className="align-middle">
           <span
-            className="material-symbols-outlined"
+            className="material-symbols-outlined d-inline-block"
             style={{
               transform: `rotate(${isOpen ? 180 : 0}deg)`,
               transition: 'all 0.25s',
+              verticalAlign: 'middle',
             }}
           >
             expand_more
           </span>
-          {character.name}
+          <span className="d-inline-block" style={{ verticalAlign: 'middle' }}>
+            {character.name}
+          </span>
         </td>
         <td>{character.status}</td>
         <td>{character.species}</td>
