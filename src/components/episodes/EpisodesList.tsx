736533601edb128bf8ce32fda3a30808c249eb89
Sepideh Attar <sepideh.att@gmail.com>
2024-03-11 11:41:05 +0100

    Fixed table header in episodesList

--- src/components/episodes/EpisodesList.tsx
+++ src/components/episodes/EpisodesList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import EpisodeDetail from './EpisodeDetail';
 import { EpisodeResponse } from '../../interfaces/episodes.interface';
+import './episodes.style.scss';
 
 interface EpisodesListProps {
   episodes: EpisodeResponse[];
@@ -20,14 +21,13 @@ const EpisodesList: React.FC<EpisodesListProps> = ({
     <div className="col-md-6 ps-4">
       <div
         className="table-responsive"
-        id="scrollable-table"
-        style={{ maxHeight: '300px', overflow: 'auto' }}
+        style={{ overflowY: 'auto', maxHeight: '250px' }}
       >
         <table
           className="table table-sm table-striped mx-auto my-3"
           style={{ tableLayout: 'fixed' }}
         >
-          <thead>
+          <thead className="sticky-top-th">
             <tr>
               <th className="w-50">Name</th>
               <th>Season</th>
@@ -46,7 +46,7 @@ const EpisodesList: React.FC<EpisodesListProps> = ({
             ))}
             {loading && (
               <tr>
-                <td>Loading more episodes...</td>
+                <td colSpan={3}>Loading more episodes...</td>
               </tr>
             )}
           </tbody>
